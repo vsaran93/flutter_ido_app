2cb0fbc5bf3b4c2a80ede62f0d5f6d878c11f3fb
vsaran93 <vishnusaran37@gmail.com>
2025-06-15 13:00:18 +0100

    bottom nav bar changes

--- lib/main_navigator.dart
+++ lib/main_navigator.dart
@@ -28,7 +28,6 @@ class MainNavigator extends StatefulWidget {
 
 
 class _MainNavigator extends State<MainNavigator> {
-  final tabs = [ HomeScreen(), ActivitiesScreen(), NotificationScreen(), AccountScreen() ];
   int currentPageIndex = 0;
 
   Map<int, GlobalKey<NavigatorState>>navigatorKeys = {
@@ -51,6 +50,38 @@ class _MainNavigator extends State<MainNavigator> {
     );
   }
  
+  Widget _buildNavItem({required IconData icon, required String label, required int index}) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
+      child: Container(
+        width: 60,
+        height: double.infinity,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 20,
+              color: currentPageIndex == index ? Colors.white : Colors.grey
+            ),
+            SizedBox(height: 4,),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                color: currentPageIndex == index ? Colors.white : Colors.grey
+              ),
+            )
+          ],)
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,10 +89,10 @@ class _MainNavigator extends State<MainNavigator> {
         child: PopScope(
           onPopInvoked: (didPop) {
             if(!didPop) {
-              final context1 = navigatorKeys[currentPageIndex]?.currentContext;
-              if (context1 != null) {
-                Navigator.maybePop(context1);
-              }  
+              final currentNavigationState = navigatorKeys[currentPageIndex]?.currentState;
+              if (currentNavigationState?.canPop() ?? false) {
+                currentNavigationState?.pop();
+              }
             }
           },
           child: IndexedStack(
@@ -75,39 +106,35 @@ class _MainNavigator extends State<MainNavigator> {
           )
         )
       ),
-      bottomNavigationBar: Container(
-        child: BottomNavigationBar(
-          backgroundColor: Colors.blue.shade800,
-          currentIndex: currentPageIndex,
-          unselectedItemColor: Colors.grey,
-          selectedItemColor: Colors.white,
-          type: BottomNavigationBarType.fixed,
-          elevation: 0,
-          onTap: (int index) {
-            setState(() {
-              currentPageIndex = index;
-            });
-          },
-          items: const<BottomNavigationBarItem> [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home), 
-              label: 'Home'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.apps), 
-              label: 'Activities'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications), 
-            label: 'Notifications'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person), 
-            label: 'Account'
-          )
-          ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue.shade800, // Button background color
+        foregroundColor: Colors.white,  // Icon color
+        elevation: 6,  
+        child: const Icon(Icons.add),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(26), // Custom rounded shape
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 6, // Space between button and bar
+          color: Colors.blue.shade800,
+          child: SizedBox(
+            height: 64,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children:  [
+                _buildNavItem(icon: Icons.home, label: 'Home', index: 0),
+                _buildNavItem(icon: Icons.apps, label: 'Activities', index: 1),
+                const SizedBox(width: 40),
+                _buildNavItem(icon: Icons.notifications, label: 'Alerts', index: 2),
+                _buildNavItem(icon: Icons.person, label: 'Account', index: 3),
+                ],
+            )
+        ),
+        )
     );
   }
 }
